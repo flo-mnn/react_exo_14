d43f7e96be27b1c3292e698b9815d77d0dbd8aa4
Florence <monnaie.florence@gmail.com>
2021-01-19 09:20:59 +0100

    exo14 done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, {useState} from 'react';
+import Veg from './Veg';
 import './App.css';
 
 function App() {
 
-  const [fruit, setfruit] = useState('la fraise');
+  const [fruit, setFruit] = useState('la fraise');
+  const [veg, setVeg] = useState('la carotte');
 
   return (
     <div className="App">
       <h1>Mon fruit préféré est {fruit}</h1>
+      <Veg veg={veg}/>
     </div>
   );
 }
---
+++ src/Veg.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+
+let Veg = ({veg}) => {
+
+    return(
+        <h5>Mon légume préféré est {veg}</h5>
+    );
+};
+
+export default Veg
